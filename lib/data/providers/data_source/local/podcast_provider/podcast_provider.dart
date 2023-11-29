5d98c8d35ea6c913f835216d50f995dfe59a9d3f
abolfazl <abolfazlmashhadi93@gmail.com>
2023-11-29 13:58:12 +0330

    added podcast provider

---
+++ lib/data/providers/data_source/local/podcast_provider/podcast_provider.dart
@@ -0,0 +1,20 @@
+import 'package:podcast_search/podcast_search.dart' as podcast;
+
+class PodcastProvider {
+  /// load List<Item> from chart
+  Future<podcast.SearchResult> loadChartFeed() async {
+    //country default iran
+    podcast.Country country = podcast.Country.iran;
+    return await podcast.Search().charts(country: country, limit: 20);
+  }
+
+  /// load podcast from feed url
+  Future<podcast.Podcast> loadPodcastFromFeed({required String feedUrl}) async {
+    return await podcast.Podcast.loadFeed(url: feedUrl);
+  }
+
+  /// search podcast by term sent
+  Future<podcast.SearchResult>searchPodcast({required String query})async{
+    return await podcast.Search().search(query);
+  }
+}
